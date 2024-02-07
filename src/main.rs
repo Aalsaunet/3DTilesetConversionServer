580f28e74cdf31650218097d4ca67e46976cc7df
Andreas Oven Aalsaunet <andreas.aalsaunet@gmail.com>
2024-02-07 12:36:41 +0100

    Added the norkart url

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+// const NORKART_URL: &str = "https://waapi.webatlas.no/3d-tiles/tileserver.fcgi/tileset.json?api_key=DB124B20-9D21-4647-B65A-16C651553E48";
+const NORKART_URL: &str = "https://waapi.webatlas.no/3d-tiles/tileserver.fcgi/tileset.json?api_key=";
+const NORKART_API_KEY: &str = "DB124B20-9D21-4647-B65A-16C651553E48";
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
@@ -22,12 +26,53 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
+    
+    println!("Request: {:#?}", http_request.first().unwrap());
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+    // let status_line = "HTTP/1.1 200 OK";
+    // let contents = fs::read_to_string("hello.html").unwrap();
+    // let length = contents.len();
+
+    // let response =
+    //     format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    // stream.write_all(response.as_bytes()).unwrap();
 }
+
+// fn handle_connection(mut stream: TcpStream) {
+//     let buf_reader = BufReader::new(&mut stream);
+//     let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+//     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+//         ("HTTP/1.1 200 OK", "hello.html")
+//     } else {
+//         ("HTTP/1.1 404 NOT FOUND", "404.html")
+//     };
+
+//     let contents = fs::read_to_string(filename).unwrap();
+//     let length = contents.len();
+
+//     let response =
+//         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+//     stream.write_all(response.as_bytes()).unwrap();
+// }
+
+// fn handle_connection(mut stream: TcpStream) {
+//     let buf_reader = BufReader::new(&mut stream);
+//     let http_request: Vec<_> = buf_reader
+//         .lines()
+//         .map(|result| result.unwrap())
+//         .take_while(|line| !line.is_empty())
+//         .collect();
+
+//     let status_line = "HTTP/1.1 200 OK";
+//     let contents = fs::read_to_string("hello.html").unwrap();
+//     let length = contents.len();
+
+//     let response =
+//         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+//     stream.write_all(response.as_bytes()).unwrap();
+// }
